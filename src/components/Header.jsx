b32cb2873af606bbd04dc696fb0ebb51fd191331
muskan0729 <muskan@spay.live>
2025-09-30 18:22:20 +0530

    designed profile icon in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,32 @@
 import React from "react";
+import logo from "../images/logo.png";
+import { useState, useEffect, useRef } from "react";
 
 export const Header = () => {
+
+  const [open, setOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+
+
   return (
-    <nav class="bg-white border-gray-200 ">
+    <nav class="bg-white border-red-100 h-20 shadow-lg shadow-indigo-500/50">
       <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a
-          href="https://flowbite.com/"
-          class="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            class="h-8"
-            alt="Flowbite Logo"
-          />
-          <span class="self-center text-2xl font-semibold whitespace-nowrap ">
-            Flowbite
-          </span>
+        <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src={logo} class="h-12" alt="Spay Logo" />
         </a>
         <button
           data-collapse-toggle="navbar-default"
@@ -24,7 +35,7 @@ export const Header = () => {
           aria-controls="navbar-default"
           aria-expanded="false"
         >
-          <span class="sr-only">Open main menu</span>
+          <span class="sr-only"></span>
           <svg
             class="w-5 h-5"
             aria-hidden="true"
@@ -43,46 +54,46 @@ export const Header = () => {
         </button>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
           <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white ">
-            <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 "
-                aria-current="page"
+            <li className="relative" ref={dropdownRef}>
+              <button
+                onClick={() => setOpen(!open)}
+                class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
               >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 "
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </a>
+                <img
+                  className="w-15 h-15"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRduYoJopcD2_WmDjt978P3pjTLl-oQX-ZsTOaof805POhNgFzpYEy5LnA&s"
+                  alt="profile image"
+                />
+                {/* <i class="fa-solid fa-user"></i> */}
+              </button>
+              {open && (
+                <ul className="absolute right-0 mt-3 w-68 shadow-xl/30 z-50  px-4 py-4 rounded-lg"  style={{ backgroundColor: "#A7B7F1" }}>
+                  <div className="bg-gray-100 rounded-lg">
+                  <div className="text-center text-gray-700">
+                    <h6>username</h6>
+                     <h6>username@gmail.com</h6><hr />
+                  </div>
+                  <li>
+                    <a
+                      href="#profile"
+                      className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                    >
+                      Profile
+                    </a>
+                  </li>
+                  
+                  
+                  <li>
+                    <a
+                      href="#logout"
+                      className="block px-4 py-2 text-gray-700 hover:bg-gray-100"
+                    >
+                      Logout
+                    </a>
+                  </li>
+                  </div>
+                </ul>
+              )}
             </li>
           </ul>
         </div>
