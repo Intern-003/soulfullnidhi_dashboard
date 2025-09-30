da8bb3109c6ab8388a287f199710e544588f026f
Intern <intern@spay.live>
2025-09-30 15:55:40 -0700

    feat:added logo in sidebar and added icon of menu

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import Logo from "../images/logo.png"
 export const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -9,16 +9,13 @@ export const Sidebar = () => {
 
   return (
     <>
-      <div class="text-center">
+      <div>
         <button
-          class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5"
+          class="font-medium rounded-lg text-sm px-5 py-2.5"
           type="button"
-          data-drawer-target="drawer-navigation"
-          data-drawer-show="drawer-navigation"
-          aria-controls="drawer-navigation"
           onClick={toggleSidebar}
         >
-          Show navigation
+          <i class="fa-solid fa-bars fa-2xl"></i>
         </button>
       </div>
 
@@ -28,12 +25,7 @@ export const Sidebar = () => {
         tabindex="-1"
         aria-labelledby="drawer-navigation-label"
       >
-        <h5
-          id="drawer-navigation-label"
-          class="text-base font-semibold text-gray-500 uppercase"
-        >
-          Menu
-        </h5>
+        <img src={Logo} alt="" />
         <button
           type="button"
           data-drawer-hide="drawer-navigation"
@@ -81,19 +73,7 @@ export const Sidebar = () => {
                 href="#"
                 class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group"
               >
-                <svg
-                  class="shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 18 18"
-                >
-                  <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z" />
-                </svg>
                 <span class="flex-1 ms-3 whitespace-nowrap">Kanban</span>
-                <span class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full">
-                  Pro
-                </span>
               </a>
             </li>
             <li>
