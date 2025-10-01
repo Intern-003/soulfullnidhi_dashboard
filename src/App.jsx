9fccd5a0e7a79a48c09529ef5d4894bd2577cbfd
muskan0729 <muskan@spay.live>
2025-10-01 16:11:41 +0530

    Merged sidebar and header in layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { Header } from "./components/Header";
-import { Sidebar } from "./components/Sidebar";
+// import { Header } from "./components/Header";
+// import { Sidebar } from "./components/Sidebar";
 import { Routes, Route } from 'react-router-dom';
 import Payoutrequest from './pages/Payoutrequest';
+import Scheme from './pages/Scheme';
+import Layout from "./components/Layout";
+
+
 
 function App() {
   return (
     <>
-    <Header />
-    <Sidebar />
+    {/* <Header />
+    <Sidebar /> */}
+    {/* <Layout> */}
       <div className="main-content h-screen">
         <Routes>
           <Route path="/payout-request" element={<Payoutrequest />} />
+          <Route path="/Scheme" element={<Scheme />} />
         </Routes>
       </div>
+      {/* </Layout> */}
     </>
   );
 }
