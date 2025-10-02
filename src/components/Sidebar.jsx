24e8e1a382da4708c41e6d28e4d91e3409f69597
Intern <intern@spay.live>
2025-10-01 17:48:18 -0700

    feat:added icons in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -37,16 +37,7 @@ export const Sidebar = () => {
                 href="#"
                 class="flex items-center p-2 text-white rounded-lg hover:bg-blue-200 group hover:text-blue-900"
               >
-                <svg
-                  class="w-5 h-5 text-white transition duration-75 group-hover:text-blue-900"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 22 21"
-                >
-                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
-                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
-                </svg>
+                <i class="fa-solid fa-chart-pie fa-lg text-white transition duration-75 group-hover:text-blue-900"></i>
                 <span class="ms-3">Dashboard</span>
               </a>
             </li>
@@ -58,7 +49,11 @@ export const Sidebar = () => {
                 type="button"
                 onClick={() => setSchemeDropDown(!schemeDropDown)}
               >
-                <span className="ms-3">Scheme Manager</span>
+                <div>
+                  <i class="fa-solid fa-money-check fa-lg text-white transition duration-75 group-hover:text-blue-900"></i>
+                  <span className="ms-3">Scheme Manager</span>
+                </div>
+
                 <svg
                   class="w-2.5 h-2.5 ms-3"
                   aria-hidden="true"
@@ -87,7 +82,9 @@ export const Sidebar = () => {
             >
               <ul>
                 <li class="text-gray-900 hover:text-white hover:bg-blue-900 p-1">
-                  <a href="">Scheme</a>
+                  <a href="">
+                    <span className="ms-3">Scheme</span>
+                  </a>
                 </li>
               </ul>
             </div>
@@ -101,7 +98,11 @@ export const Sidebar = () => {
                 type="button"
                 onClick={() => setMemberDropDown(!memberDropDown)}
               >
-                <span className="ms-3">Member</span>
+                <div>
+                  <i class="fa-solid fa-user-group fa-lg text-white transition duration-75 group-hover:text-blue-900"></i>
+                  <span className="ms-3">Member</span>
+                </div>
+
                 <svg
                   class="w-2.5 h-2.5 ms-3"
                   aria-hidden="true"
@@ -144,7 +145,10 @@ export const Sidebar = () => {
                 type="button"
                 onClick={() => setFundDropDown(!fundDropDown)}
               >
-                <span className="ms-3">Fund</span>
+                <div>
+                  <i class="fa-solid fa-piggy-bank fa-lg text-white transition duration-75 group-hover:text-blue-900"></i>
+                  <span className="ms-3">Fund</span>
+                </div>
                 <svg
                   class="w-2.5 h-2.5 ms-3"
                   aria-hidden="true"
@@ -190,7 +194,10 @@ export const Sidebar = () => {
                 type="button"
                 onClick={() => setPayoutDropDown(!payoutDropDown)}
               >
-                <span className="ms-3">Payout</span>
+                <div>
+                  <i class="fa-solid fa-credit-card fa-lg text-white transition duration-75 group-hover:text-blue-900"></i>
+                  <span className="ms-3">Payout</span>
+                </div>
                 <svg
                   class="w-2.5 h-2.5 ms-3"
                   aria-hidden="true"
@@ -233,7 +240,10 @@ export const Sidebar = () => {
                 type="button"
                 onClick={() => setPayinDropDown(!payinDropDown)}
               >
-                <span className="ms-3">Payin</span>
+                <div>
+                  <i class="fa-solid fa-money-bill-transfer fa-lg text-white transition duration-75 group-hover:text-blue-900"></i>
+                  <span className="ms-3">Payin</span>
+                </div>
                 <svg
                   class="w-2.5 h-2.5 ms-3"
                   aria-hidden="true"
@@ -276,7 +286,10 @@ export const Sidebar = () => {
                 type="button"
                 onClick={() => setTransactionDropDown(!transactionDropDown)}
               >
-                <span className="ms-3">Transaction History</span>
+                <div>
+                  <i class="fa-solid fa-clock-rotate-left fa-lg text-white transition duration-75 group-hover:text-blue-900"></i>
+                  <span className="ms-3">Transaction History</span>
+                </div>
                 <svg
                   class="w-2.5 h-2.5 ms-3"
                   aria-hidden="true"
@@ -322,7 +335,10 @@ export const Sidebar = () => {
                 type="button"
                 onClick={() => setAccountDropDown(!accountDropDown)}
               >
-                <span className="ms-3">Account Statement</span>
+                <div>
+                  <i class="fa-solid fa-layer-group fa-lg text-white transition duration-75 group-hover:text-blue-900"></i>
+                  <span className="ms-3">Account Statement</span>
+                </div>
                 <svg
                   class="w-2.5 h-2.5 ms-3"
                   aria-hidden="true"
@@ -368,7 +384,10 @@ export const Sidebar = () => {
                 type="button"
                 onClick={() => setRolesDropDown(!rolesDropDown)}
               >
-                <span className="ms-3">Roles & Permissions</span>
+                <div>
+                  <i class="fa-solid fa-gears fa-lg text-white transition duration-75 group-hover:text-blue-900"></i>
+                  <span className="ms-2">Roles & Permissions</span>
+                </div>
                 <svg
                   class="w-2.5 h-2.5 ms-3"
                   aria-hidden="true"
@@ -414,7 +433,10 @@ export const Sidebar = () => {
                 type="button"
                 onClick={() => setTicketsDropDown(!ticketsDropDown)}
               >
-                <span className="ms-3">Complaints</span>
+                <div>
+                  <i class="fa-solid fa-comment fa-lg text-white transition duration-75 group-hover:text-blue-900"></i>
+                  <span className="ms-3">Complaints</span>
+                </div>
                 <svg
                   class="w-2.5 h-2.5 ms-3"
                   aria-hidden="true"
