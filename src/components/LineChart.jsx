b2206c775f9c7ad9a0105408bbff49bfc36aa0cc
muskan0729 <muskan@spay.live>
2025-11-14 16:39:22 +0530

    added image for testing

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import Nodatafound from "../images/NodataLine.jpg";
+import Nodatafound from "../images/linedata.png";
 export const LineChart = ({ data }) => {
   const chartRef = useRef(null);
   const [amount, setAmount] = useState([1]);
